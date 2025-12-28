1cadf0e76a9c33e1b0f512ab1315052a88affc89
yna87 <yna.sakana@gmail.com>
2025-12-28 22:25:07 +0900

    chore: format

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+
+# セットアップ内容
+
+Vite https://vite.dev/guide/
+Tailwind CSS https://tailwindcss.com/docs/installation/using-vite
+Eslint https://eslint.org/docs/latest/use/getting-started
+Prettier https://prettier.io/docs/install
+eslint-config-prettier https://github.com/prettier/eslint-config-prettier#installation
+Storybook
--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
+  pluginVue.configs['flat/essential'],
   {
-    files: ["**/*.vue"],
+    files: ['**/*.vue'],
     languageOptions: { parserOptions: { parser: tseslint.parser } },
   },
   eslintConfigPrettier,
-]);
+])
