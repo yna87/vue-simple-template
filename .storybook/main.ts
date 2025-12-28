023553736932a411202abb8cbff1e96f45db2491
yna87 <yna.sakana@gmail.com>
2025-12-29 02:32:41 +0900

    chore: fix storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,13 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
-  ],
-  "framework": "@storybook/vue3-vite"
-};
-export default config;
+  framework: '@storybook/vue3-vite',
+}
+export default config
