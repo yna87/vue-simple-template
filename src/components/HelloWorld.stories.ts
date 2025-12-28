5a48dabe753bd92f77ad4bd1f1e98534096589ed
yna87 <yna.sakana@gmail.com>
2025-12-29 03:14:30 +0900

    test: add counter test

--- src/components/HelloWorld.stories.ts
+++ src/components/HelloWorld.stories.ts
@@ -1,3 +1,4 @@
+import { expect, within } from '@storybook/test'
 import type { Meta, StoryObj } from '@storybook/vue3-vite'
 
 import HelloWorld from './HelloWorld.vue'
@@ -19,3 +20,31 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Basic: Story = {}
+
+// Test Counter
+export const Counter: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    // Get button elements
+    const countUpButton = await canvas.getByRole('button', { name: '+' })
+    const countDownButton = await canvas.getByRole('button', { name: '-' })
+    const resetButton = await canvas.getByRole('button', { name: 'Reset' })
+
+    // Check initial count value
+    const countValue = await canvas.getByText('Count: 0')
+    await expect(countValue).toBeInTheDocument()
+
+    // Click the count up button
+    await countUpButton.click()
+    await expect(canvas.getByText('Count: 1')).toBeInTheDocument()
+
+    // Click the reset button
+    await resetButton.click()
+    await expect(canvas.getByText('Count: 0')).toBeInTheDocument()
+
+    // Click the count down button
+    await countDownButton.click()
+    await expect(canvas.getByText('Count: -1')).toBeInTheDocument()
+  },
+}
