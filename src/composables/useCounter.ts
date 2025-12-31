e9cdbfc7ce66c7d005a61ce02bebac9541acca1b
yna87 <yna.sakana@gmail.com>
2025-12-31 16:42:22 +0900

    chore: add auto import

--- src/composables/useCounter.ts
+++ src/composables/useCounter.ts
@@ -1,5 +1,3 @@
-import { ref } from 'vue'
-
 export function useCounter(initialValue = 0) {
   const count = ref(initialValue)
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 import { playwright } from '@vitest/browser-playwright'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 const dirname =
   typeof __dirname !== 'undefined'
     ? __dirname
@@ -15,7 +16,20 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    AutoImport({
+      imports: ['vue'],
+      dirs: [
+        './src/api/**',
+        './src/components/**',
+        './src/composables/**',
+        './src/types/**',
+      ],
+      dts: './src/auto-imports.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(dirname, './src'),
